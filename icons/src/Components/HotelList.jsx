51751b3e2669b601d46bab0d3c4a02c314264798
dishanayak383 <dishanayak383@gmail.com>
2025-02-21 12:50:02 +0530

    first commit

---
+++ icons/src/Components/HotelList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./HotelList.css";
+import { IoSearch } from "react-icons/io5";
+
+
+
+const HotelList = () => {
+    return (
+        <div className="explore-hotels">
+        <div className="hotel-container">
+            <div className="head">
+                <h3>Let's</h3>
+                <a className="search"><IoSearch /></a>
+            </div>
+                <span className="highlight">Explore Hotels</span>
+      
+
+        </div>
+
+        </div>
+            
+                
+    )
+}   
+
+      
+                
+ 
---
+++ icons/src/Components/HotelList.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import HotelList from "./Hotel.jsx";
+
+export default {
+  title: "Hotel List",
+  component: HotelList,
+};
+
+export const Default = () => <HotelList />;
